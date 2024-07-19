832008b7741c10e13f82111f552c9d83f4ec350f
IBRAHIM <102235503+ibrpride@users.noreply.github.com>
2024-07-19 10:44:51 +0300

    Update Install.NvidiaControlPanel.bat

--- Install.NvidiaControlPanel.bat
+++ Install.NvidiaControlPanel.bat
@@ -1,20 +1,28 @@
 @echo off
-title Install Control Panel
-color 02
-mode 120,50
+title Installing NVIDIA Control Panel
+color 2
 
+
+echo Installing NVIDIA Control Panel...
+echo.
+
+:: Download files with progress indicator
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ibrpride/DCH-Drivers-Control-Panel/releases/download/nvcplui/nvcpl.dll', '%appdata%\nvcpl.dll')"
+echo File 1/2 downloaded.
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ibrpride/DCH-Drivers-Control-Panel/releases/download/nvcplui/nvcplui.exe', '%appdata%\nvcplui.exe')"
+echo File 2/2 downloaded.
+
+:: Set up registry entries
+echo Setting up registry entries...
+reg delete "HKEY_CLASSES_ROOT\Directory\Background\ShellEx\ContextMenuHandlers\NvCplDesktopContext" /f >nul 2>&1
+reg add "HKCR\Directory\Background\shell\Item0" /v "MUIVerb" /t REG_SZ /d "NVIDIA Control Panel" /f >nul 2>&1
+reg add "HKCR\Directory\Background\shell\Item0" /v "Icon" /t REG_SZ /d "%appdata%\nvcpl.dll,0" /f >nul 2>&1
+reg add "HKCR\Directory\Background\shell\Item0\command" /ve /t REG_SZ /d "%appdata%\nvcplui.exe" /f >nul 2>&1
+
+:: Disable Store Install Control Panel Notifications
+echo Disabling Store Install Control Panel Notifications...
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\nvlddmkm\Global\NVTweak" /v DisableStoreNvCplNotifications /t REG_DWORD /d 1 /f >nul 2>&1
 
 echo.
-timeout 3 /nobreak >nul
-cls
-powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/ibrpride/DCH-Drivers-Control-Panel/releases/download/nvcplui/nvcpl.dll','%appdata%\nvcpl.dll')
-powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/ibrpride/DCH-Drivers-Control-Panel/releases/download/nvcplui/nvcplui.exe','%appdata%\nvcplui.exe')
-reg delete "HKEY_CLASSES_ROOT\Directory\Background\ShellEx\ContextMenuHandlers\NvCplDesktopContext" /f 
-reg add "HKCR\Directory\Background\shell\Item0" /v "MUIVerb" /t REG_SZ /d "NVIDIA Control Panel" /f
-reg add "HKCR\Directory\Background\shell\Item0" /v "Icon" /t REG_SZ /d "%appdata%\nvcpl.dll,0" /f
-reg add "HKCR\Directory\Background\shell\Item0\command" /ve /t REG_SZ /d "%appdata%\nvcplui.exe" /f
-
-echo Disable Store Install Control Panel Notifications
-
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\nvlddmkm\Global\NVTweak" /v DisableStoreNvCplNotifications /t REG_DWORD /d 1 /f
+echo Installation complete.
 timeout 3 /nobreak >nul
